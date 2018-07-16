65127e7366b360df43362583e4ac29cfbf42d4fe
huangmingchuan <hmcxxdxx@gmail.com>
2018-07-16 20:48:03 +0800

    001

--- LeetCode/Solution.h
+++ LeetCode/Solution.h
@@ -1,9 +1,28 @@
 #pragma once
+
 #include <vector>
 #include <set>
 
-class Solution
+using namespace std;
+
+class Solution001
 {
 public:
+	vector<int> twoSum(vector<int>& nums, int target)
+	{
+		vector<int> result;
+		for (int i = 0; i < nums.size() - 1; ++i)
+		{
+			for (int j = i + 1; j < nums.size(); ++j)
+			{
+				if (nums[i] + nums[j] == target)
+				{
+					result.push_back(i);
+					result.push_back(j);
+				}
+			}
+		}
 
+		return result;
+	}
 };
