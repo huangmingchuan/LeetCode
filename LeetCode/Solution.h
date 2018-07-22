429400f192d446274846d2b0eb47eaaa9682678c
huangmingchuan <hmcxxdxx@gmail.com>
2018-07-22 22:49:00 +0800

    add isEqualList function and Testcase

--- LeetCode/Solution.h
+++ LeetCode/Solution.h
@@ -5,15 +5,40 @@
 
 using namespace std;
 
+struct ListNode
+{
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(nullptr) {}
+};
+
+bool isEqualList(ListNode *l1, ListNode *l2)
+{
+	while (l1 && l2)
+	{
+		if (l1->val != l2->val)
+		{
+			return false;
+		}
+		l1 = l1->next;
+		l2 = l2->next;
+	}
+
+	if ((l1 == nullptr) ^ (l2 == nullptr))
+		return false;
+
+	return true;
+}
+
 class Solution001
 {
 public:
 	vector<int> twoSum(vector<int>& nums, int target)
 	{
 		vector<int> result;
-		for (int i = 0; i < nums.size() - 1; ++i)
+		for (size_t i = 0; i < nums.size() - 1; ++i)
 		{
-			for (int j = i + 1; j < nums.size(); ++j)
+			for (size_t j = i + 1; j < nums.size(); ++j)
 			{
 				if (nums[i] + nums[j] == target)
 				{
@@ -24,5 +49,14 @@ public:
 		}
 
 		return result;
+	}
+};
+
+class Solution002
+{
+public:
+	ListNode * addTwoNumber(ListNode* l1, ListNode* l2)
+	{
+
 	}
 };
