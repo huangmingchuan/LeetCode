bed0438f41740e9bbad7ef6ea185b90cfe61ba29
huangmingchuan <hmcxxdxx@gmail.com>
2018-07-24 00:07:25 +0800

    replace tab to space

--- LeetCode/Solution.h
+++ LeetCode/Solution.h
@@ -7,89 +7,89 @@ using namespace std;
 
 struct ListNode
 {
-	int val;
-	ListNode *next;
-	ListNode(int x) : val(x), next(nullptr) {}
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
 };
 
 bool isEqualList(ListNode *l1, ListNode *l2)
 {
-	while (l1 && l2)
-	{
-		if (l1->val != l2->val)
-		{
-			return false;
-		}
-		l1 = l1->next;
-		l2 = l2->next;
-	}
+    while (l1 && l2)
+    {
+        if (l1->val != l2->val)
+        {
+            return false;
+        }
+        l1 = l1->next;
+        l2 = l2->next;
+    }
 
-	if ((l1 == nullptr) ^ (l2 == nullptr))
-		return false;
+    if ((l1 == nullptr) ^ (l2 == nullptr))
+        return false;
 
-	return true;
+    return true;
 }
 
 class Solution001
 {
 public:
-	vector<int> twoSum(vector<int>& nums, int target)
-	{
-		vector<int> result;
-		for (size_t i = 0; i < nums.size() - 1; ++i)
-		{
-			for (size_t j = i + 1; j < nums.size(); ++j)
-			{
-				if (nums[i] + nums[j] == target)
-				{
-					result.push_back(i);
-					result.push_back(j);
-				}
-			}
-		}
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        vector<int> result;
+        for (size_t i = 0; i < nums.size() - 1; ++i)
+        {
+            for (size_t j = i + 1; j < nums.size(); ++j)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    result.push_back(i);
+                    result.push_back(j);
+                }
+            }
+        }
 
-		return result;
-	}
+        return result;
+    }
 };
 
 class Solution002
 {
 public:
-	ListNode * addTwoNumber(ListNode* l1, ListNode* l2)
-	{
-		ListNode *result = nullptr;
-		int num = listToNumber(l1) + listToNumber(l2);
-		result = numberToList(num);
-		return result;
-	}
+    ListNode * addTwoNumber(ListNode* l1, ListNode* l2)
+    {
+        ListNode *result = nullptr;
+        int num = listToNumber(l1) + listToNumber(l2);
+        result = numberToList(num);
+        return result;
+    }
 
 private:
-	int listToNumber(ListNode *l)
-	{
-		int result = 0, base = 1;
+    int listToNumber(ListNode *l)
+    {
+        int result = 0, base = 1;
 
-		while (l)
-		{
-			result = result + base * l->val;
-			base *= 10;
-			l = l->next;
-		}
+        while (l)
+        {
+            result = result + base * l->val;
+            base *= 10;
+            l = l->next;
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	ListNode * numberToList(int num)
-	{
-		ListNode head(0);
-		ListNode *p = &head;
+    ListNode * numberToList(int num)
+    {
+        ListNode head(0);
+        ListNode *p = &head;
 
-		while (num)
-		{
-			p->next = new ListNode(num % 10);
-			p = p->next;
-			num /= 10;
-		}
-		
-		return head.next;
-	}
+        while (num)
+        {
+            p->next = new ListNode(num % 10);
+            p = p->next;
+            num /= 10;
+        }
+
+        return head.next;
+    }
 };
--- LeetCode/testcase.cpp
+++ LeetCode/testcase.cpp
@@ -7,163 +7,163 @@ using namespace std;
 
 TEST_CASE("isEqualList")
 {
-	SECTION("one")
-	{
-		//todo: 改为 list，而非手写 ListNode
-		ListNode node1(1);
-		ListNode node2(2);
-		ListNode node3(3);
-		node1.next = &node2;
-		node2.next = &node3;
+    SECTION("one")
+    {
+        //todo: 改为 list，而非手写 ListNode
+        ListNode node1(1);
+        ListNode node2(2);
+        ListNode node3(3);
+        node1.next = &node2;
+        node2.next = &node3;
 
-		ListNode node4(1);
-		ListNode node5(2);
-		ListNode node6(3);
-		node4.next = &node5;
-		node5.next = &node6;
+        ListNode node4(1);
+        ListNode node5(2);
+        ListNode node6(3);
+        node4.next = &node5;
+        node5.next = &node6;
 
-		auto &l1 = node1;
-		auto &l2 = node4;
+        auto &l1 = node1;
+        auto &l2 = node4;
 
-		REQUIRE(isEqualList(&l1, &l2));
-	}
+        REQUIRE(isEqualList(&l1, &l2));
+    }
 
-	SECTION("two")
-	{
-		ListNode node1(1);
-		ListNode node2(2);
-		ListNode node3(3);
-		node1.next = &node2;
-		node2.next = &node3;
+    SECTION("two")
+    {
+        ListNode node1(1);
+        ListNode node2(2);
+        ListNode node3(3);
+        node1.next = &node2;
+        node2.next = &node3;
 
-		ListNode node4(4);
-		ListNode node5(5);
-		ListNode node6(6);
-		node4.next = &node5;
-		node5.next = &node6;
+        ListNode node4(4);
+        ListNode node5(5);
+        ListNode node6(6);
+        node4.next = &node5;
+        node5.next = &node6;
 
-		auto &l1 = node1;
-		auto &l2 = node4;
+        auto &l1 = node1;
+        auto &l2 = node4;
 
-		REQUIRE_FALSE(isEqualList(&l1, &l2));
-	}
+        REQUIRE_FALSE(isEqualList(&l1, &l2));
+    }
 
-	SECTION("three")
-	{
-		ListNode node1(1);
-		ListNode node2(2);
-		ListNode node3(3);
-		node1.next = &node2;
-		node2.next = &node3;
+    SECTION("three")
+    {
+        ListNode node1(1);
+        ListNode node2(2);
+        ListNode node3(3);
+        node1.next = &node2;
+        node2.next = &node3;
 
-		ListNode node4(1);
-		ListNode node5(2);
-		ListNode node6(2);
-		node4.next = &node5;
-		node5.next = &node6;
+        ListNode node4(1);
+        ListNode node5(2);
+        ListNode node6(2);
+        node4.next = &node5;
+        node5.next = &node6;
 
-		auto &l1 = node1;
-		auto &l2 = node4;
+        auto &l1 = node1;
+        auto &l2 = node4;
 
-		REQUIRE_FALSE(isEqualList(&l1, &l2));
-	}
+        REQUIRE_FALSE(isEqualList(&l1, &l2));
+    }
 
-	SECTION("four")
-	{
-		ListNode *node1 = nullptr;
-		ListNode *node2 = nullptr;
+    SECTION("four")
+    {
+        ListNode *node1 = nullptr;
+        ListNode *node2 = nullptr;
 
-		REQUIRE(isEqualList(node1, node2));
-	}
+        REQUIRE(isEqualList(node1, node2));
+    }
 }
 
 TEST_CASE("Solution001")
 {
-	SECTION("one")
-	{
-		Solution001 s;
-		vector<int> nums{ 1,3,5,9 };
-		int target = 10;
-		vector<int> result{ 0,3 };
+    SECTION("one")
+    {
+        Solution001 s;
+        vector<int> nums{ 1,3,5,9 };
+        int target = 10;
+        vector<int> result{ 0,3 };
 
-		REQUIRE(result == s.twoSum(nums, target));
-	}
+        REQUIRE(result == s.twoSum(nums, target));
+    }
 
-	SECTION("two")
-	{
-		Solution001 s;
-		vector<int> nums{ 1,3,5,9 };
-		int target = 8;
-		vector<int> result{ 1,2 };
+    SECTION("two")
+    {
+        Solution001 s;
+        vector<int> nums{ 1,3,5,9 };
+        int target = 8;
+        vector<int> result{ 1,2 };
 
-		REQUIRE(result == s.twoSum(nums, target));
-	}
+        REQUIRE(result == s.twoSum(nums, target));
+    }
 }
 
 TEST_CASE("Solution002")
 {
-	SECTION("one")
-	{
-		Solution002 s;
-		ListNode node3(3);
-		ListNode node4(4);
-		ListNode node2(2);
-		node2.next = &node4;
-		node4.next = &node3;
-		auto &l1 = node2;
+    SECTION("one")
+    {
+        Solution002 s;
+        ListNode node3(3);
+        ListNode node4(4);
+        ListNode node2(2);
+        node2.next = &node4;
+        node4.next = &node3;
+        auto &l1 = node2;
 
-		ListNode node44(4);
-		ListNode node6(6);
-		ListNode node5(5);
-		node5.next = &node6;
-		node6.next = &node44;
-		auto &l2 = node5;
+        ListNode node44(4);
+        ListNode node6(6);
+        ListNode node5(5);
+        node5.next = &node6;
+        node6.next = &node44;
+        auto &l2 = node5;
 
-		ListNode nodea(8);
-		ListNode nodeb(0);
-		ListNode nodec(7);
-		nodec.next = &nodeb;
-		nodeb.next = &nodea;
+        ListNode nodea(8);
+        ListNode nodeb(0);
+        ListNode nodec(7);
+        nodec.next = &nodeb;
+        nodeb.next = &nodea;
 
-		auto result = s.addTwoNumber(&l1, &l2);
+        auto result = s.addTwoNumber(&l1, &l2);
 
-		REQUIRE(isEqualList(result, &nodec));
+        REQUIRE(isEqualList(result, &nodec));
 
-	}
+    }
 
-	SECTION("two")
-	{
-		//todo: 也要用 list 改
-		Solution002 s;
-		ListNode node3(3);
-		ListNode node4(4);
-		ListNode node2(2);
-		node2.next = &node4;
-		node4.next = &node3;
-		auto &l1 = node2;
+    SECTION("two")
+    {
+        //todo: 也要用 list 改
+        Solution002 s;
+        ListNode node3(3);
+        ListNode node4(4);
+        ListNode node2(2);
+        node2.next = &node4;
+        node4.next = &node3;
+        auto &l1 = node2;
 
-		ListNode node44(4);
-		ListNode node6(6);
-		ListNode node5(5);
-		node5.next = &node6;
-		node6.next = &node44;
-		auto &l2 = node5;
+        ListNode node44(4);
+        ListNode node6(6);
+        ListNode node5(5);
+        node5.next = &node6;
+        node6.next = &node44;
+        auto &l2 = node5;
 
-		ListNode nodea(8);
-		ListNode nodeb(0);
-		ListNode nodec(9);
-		nodec.next = &nodeb;
-		nodeb.next = &nodea;
+        ListNode nodea(8);
+        ListNode nodeb(0);
+        ListNode nodec(9);
+        nodec.next = &nodeb;
+        nodeb.next = &nodea;
 
-		auto result = s.addTwoNumber(&l1, &l2);
+        auto result = s.addTwoNumber(&l1, &l2);
 
-		REQUIRE_FALSE(isEqualList(result, &nodec));
-	}
+        REQUIRE_FALSE(isEqualList(result, &nodec));
+    }
 
-	SECTION("three")
-	{
-		Solution002 s;
-		ListNode *l1 = nullptr, *l2 = nullptr;
-		REQUIRE(isEqualList(l1, l2));
-	}
+    SECTION("three")
+    {
+        Solution002 s;
+        ListNode *l1 = nullptr, *l2 = nullptr;
+        REQUIRE(isEqualList(l1, l2));
+    }
 }
